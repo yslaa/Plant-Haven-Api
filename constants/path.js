ccdbf730b1406fa69dd39f441cff61e58fd2b5f0
yslaa <crislhan.fabila@tup.edu.ph>
2023-10-16 21:05:05 +0800

    add path constant

---
+++ constants/path.js
@@ -0,0 +1,11 @@
+module.exports = {
+  LOGIN: "/login",
+  REFRESH: "/refresh",
+  LOGOUT: "/logout",
+  USERS: "/users",
+  USER_ID: "/user/:id",
+  EDIT_USER_ID: "/user/edit/:id",
+  RESET_PASSWORD: "/password/reset/:id",
+  FORGOT_PASSWORD: "/forgotPassword",
+  UPDATE_PASSWORD: "/updatePassword/:id",
+};
